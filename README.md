8ce3185783b25d4267ecb3fe6d26051e1d670e29
Julia <themusicalartist12@gmail.com>
2024-09-05 10:44:50 -0700

    Add credits

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - [Jason Culbertson](https://github.com/JTCulbertson)
 - [Sohan Lama](https://github.com/Soohann)
 - [Clayton Slack](https://github.com/Harpy88)
+
+# Attributes
+- [Piggy](https://caz-creates-games.itch.io/piggy): *caz-creates-games*
+- [Droids](https://penusbmic.itch.io/sci-fi-character-pack-6): *Penusbmic*
--- julia/characters/Piggy/Piggy.cs
+++ julia/characters/Piggy/Piggy.cs
@@ -8,10 +8,29 @@ public partial class Piggy : CharacterBody2D
 	public const float WalkSpeed = 300.0f;
 	public const float SprintSpeed = 800.0f;
 	public const float JumpVelocity = -200.0f;
+	
+	// As a percentage
+	public const int MaxHealth = 100;
+	private int Health;
 
 	float Speed = WalkSpeed;
 
-	public override void _PhysicsProcess(double delta)
+	public int getHealth() 
+	{
+		return Health;
+	}
+
+	public void takeDamage(int loss) 
+	{
+		Health -= loss;
+	}
+
+    public override void _Ready()
+    {
+		Health = MaxHealth;
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 		
@@ -40,18 +59,24 @@ public partial class Piggy : CharacterBody2D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("MainCharacterLeft", "MainCharacterRight", "NullInput", "NullInput");
-		if (direction != Vector2.Zero && IsOnFloor())
+		if (IsOnFloor()) 
 		{
-			velocity.X = direction.X * Speed;
-			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walk");
-			GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = velocity.X < 0;
-		}
-		else if (IsOnFloor())
-		{
-			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("idle");
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+			if (direction != Vector2.Zero)
+			{
+				velocity.X = direction.X * Speed;
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walk");
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = velocity.X < 0;
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").SpeedScale = Speed / WalkSpeed;
 
+			}
+			else
+			{
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("idle");
+				velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			}
+
+		}
+		
 		Velocity = velocity;
 		MoveAndSlide();
 	}

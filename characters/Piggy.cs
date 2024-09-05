aaeb538a3eef7efea414191bff25d3b39ae1250f
Julia <themusicalartist12@gmail.com>
2024-09-05 09:56:28 -0700

    movement tech improvemnts

--- characters/Piggy.cs
+++ characters/Piggy.cs
@@ -5,13 +5,16 @@ using System;
 
 public partial class Piggy : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float WalkSpeed = 300.0f;
+	public const float SprintSpeed = 800.0f;
 	public const float JumpVelocity = -200.0f;
+
+	float Speed = WalkSpeed;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
+		
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
@@ -24,19 +27,28 @@ public partial class Piggy : CharacterBody2D
 		{
 			velocity.Y = JumpVelocity;
 		}
-		
 
+
+		if (Input.IsActionPressed("MainCharacterSprint") && IsOnFloor()) 
+		{
+			Speed = Mathf.MoveToward(Speed, SprintSpeed, Speed);
+		}
+		else if (IsOnFloor())
+		{
+			Speed = Mathf.MoveToward(Speed, WalkSpeed, Speed);
+		}
+		
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("MainCharacterLeft", "MainCharacterRight", "NullInput", "NullInput");
-		if (direction != Vector2.Zero)
+		if (direction != Vector2.Zero && IsOnFloor())
 		{
 			velocity.X = direction.X * Speed;
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walk");
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = velocity.X < 0;
 
 		}
-		else
+		else if (IsOnFloor())
 		{
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("idle");
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);

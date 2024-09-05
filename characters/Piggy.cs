23992fad66bdeec5695934a83e2f55080af2eb64
Julia <themusicalartist12@gmail.com>
2024-09-05 09:39:35 -0700

    thats a main character and test scene!

---
+++ characters/Piggy.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class Piggy : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -200.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+
+		// This will be removed/modified later
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+		
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("MainCharacterLeft", "MainCharacterRight", "NullInput", "NullInput");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walk");
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = velocity.X < 0;
+
+		}
+		else
+		{
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("idle");
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
---
+++ levels/CharacterTest.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class CharacterTest : Node2D
+{
+	
+    public override void _Ready()
+    {
+		CharacterBody2D character = (CharacterBody2D)ResourceLoader.Load<PackedScene>("res://characters/piggy.tscn").Instantiate();
+		character.Position = new Vector2(500, 0);
+
+
+		AddChild(character);
+    }
+}

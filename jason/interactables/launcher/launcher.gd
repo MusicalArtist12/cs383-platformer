445165ed9c9899cd6a66f36fc95eb31f72316c09
Jason C. <jtculbertson@hotmail.com>
2024-09-05 13:54:41 -0700

    launcher v1

---
+++ jason/interactables/launcher/launcher.gd
@@ -0,0 +1,13 @@
+# Jason Culbertson
+extends Node2D
+
+@export var xVelocity: int = 10
+@export var yVelocity: int = 100
+@onready var animplayer = $AnimatedSprite2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	animplayer.play("launch")
+
+	body.velocity.y = -yVelocity*10
+	body.velocity.x = xVelocity*10

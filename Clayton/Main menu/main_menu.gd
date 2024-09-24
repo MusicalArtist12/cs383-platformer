3d9251e946edb20f0f836bdf69b81b6553228607
Julia <themusicalartist12@gmail.com>
2024-09-24 11:18:55 -0700

    merge v2

--- Clayton/Main menu/main_menu.gd
+++ Clayton/Main menu/main_menu.gd
@@ -6,15 +6,13 @@ extends Control
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartBtn as Button		#defines start button as button
 @onready var quit_button =$MarginContainer/HBoxContainer/VBoxContainer/QuitBtn as Button		#defines quit button as button
 
-
-
 func _ready():
 	start_button.button_down.connect(on_start_pressed)		#connects start button with start function
 	quit_button.button_down.connect(on_quit_pressed)		#connects quit button with quit function
 	
 	
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(Global.load_level)		#starts game
-	
+	get_tree().change_scene_to_packed(Global.main_level)
+
 func on_quit_pressed() -> void:
 	get_tree().quit()		#quits game
--- globals/global.gd
+++ globals/global.gd
@@ -3,6 +3,9 @@ extends Node
 var coins = 0 # Use Global.coins to access from any script
 signal coin_added
 
+var main_level = preload("res://Sohan/Level1.tscn") as PackedScene
+
+
 # This func will fire whenever coin is picked up, and emit coin_added
 # Use Global.connect("coin_added", self.NEWFUNCNAME) in UI script
 # to trigger NEWFUNCNAME when signal coin_added emitted

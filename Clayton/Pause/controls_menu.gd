44765e1443dfdac3ed0dc9803fa605a0e0c2f1fa
Julia <themusicalartist12@gmail.com>
2024-09-24 11:02:33 -0700

    fixes

--- Clayton/Pause/controls_menu.gd
+++ Clayton/Pause/controls_menu.gd
@@ -5,16 +5,11 @@ extends Control
 @onready var exit_btn: Button = $GridContainer3/ExitBtn as Button
 
 
-
-
 func _ready():
 	exit_btn.button_down.connect(on_exit_pressed)
 	
 
-
-
-
 func on_exit_pressed() -> void:
-	
-	get_tree().change_scene_to_packed(Global.load_level)
+	queue_free();
+	# get_tree().change_scene_to_packed(Global.load_level)
 	

ddf3c0a1cfc35670f67ffccf4a798058f13b9295
Julia <themusicalartist12@gmail.com>
2024-09-24 11:34:04 -0700

    more changes

--- globals/global.gd
+++ globals/global.gd
@@ -4,6 +4,7 @@ var coins = 0 # Use Global.coins to access from any script
 signal coin_added
 
 var main_level = preload("res://Sohan/Level1.tscn") as PackedScene
+var main_menu = preload("res://Clayton/Main menu/main_menu.tscn") as PackedScene
 
 
 # This func will fire whenever coin is picked up, and emit coin_added
